dc2e0bfc2983c79c2f83da3b2173d99fd3fc9ec4
Elf Sundae <elf.sundae@gmail.com>
2017-08-28 01:05:03 +0800

    var name

--- example/incoming.php
+++ example/incoming.php
@@ -5,7 +5,7 @@ require __DIR__.'/../vendor/autoload.php';
 use ElfSundae\BearyChat\Client;
 
 $webhook = 'https://hook.bearychat.com/=...';
-$image1 = 'http://d.hiphotos.baidu.com/image/pic/item/adaf2edda3cc7cd91ededc1a3001213fb90e9106.jpg';
+$imageUrl = 'http://d.hiphotos.baidu.com/image/pic/item/adaf2edda3cc7cd91ededc1a3001213fb90e9106.jpg';
 
 $client = new Client($webhook);
 
@@ -15,12 +15,12 @@ $client
     ->to('@elf')
     ->text('Hello, all')
     ->add('This is an attachment')
-    ->addImage($image1, 'Image Description')
+    ->addImage($imageUrl, 'Image Description')
     ->send();
 
 $client->sendTo('all',
     '**markdown** content',
     'attachment content',
     'attachment title',
-    $image1
+    $imageUrl
 );
